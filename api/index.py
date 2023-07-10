a39d7780c1159b14d951dff2b9d50e3433500735
EVR_AG <34143896+EVRAG@users.noreply.github.com>
2023-07-10 21:57:45 +0200

    simlified server

--- api/index.py
+++ api/index.py
@@ -6,19 +6,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def process_image():
-    try:
-        # Получение данных из POST-запроса
-        image = request.files['image']
-        data = request.form
+    # Получение данных из POST-запроса
+    image = request.files['image']
+    data = request.form
 
-        # Создание формы данных для отправки
-        formData = {'image': image}
+    # Создание формы данных для отправки
+    formData = {'image': image}
 
-        # Отправка POST-запроса на другой сервер
-        response = requests.post('http://178.124.214.161:7777/interrogate', files=formData)
+    # Отправка POST-запроса на другой сервер
+    response = requests.post('http://178.124.214.161:7777/interrogate', files=formData)
 
-        # Возвращение ответа от другого сервера обратно клиенту
-        return response.content, response.status_code, response.headers.items()
-    except Exception as e:
-        print(e)
-        return jsonify({'error': 'Внутренняя ошибка сервера'}), 500
+    # Возвращение ответа от другого сервера обратно клиенту
+    return response.content, response.status_code, response.headers.items()
